a974a6e310a8f4c7298a454659ed5a8208f26172
Dan Vasques <dan.vasques@outlook.com.br>
2025-01-27 19:28:14 -0300

    programa que apresenta o salario liquido do funcionario

--- src/exercicios_java_eclipse/SalarioLiquido.java
+++ src/exercicios_java_eclipse/SalarioLiquido.java
@@ -1,9 +1,34 @@
 package exercicios_java_eclipse;
 
+import java.util.Scanner;
+
 public class SalarioLiquido {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		double horasDia, precoHora, salarioBruto, salarioLiquido, desconto;
+		int diasTrabalhados;
+
+		try (Scanner sc = new Scanner(System.in)) {
+
+			System.out.print("Digite a quantidade de horas trabalhadas por dia: ");
+			horasDia = sc.nextDouble();
+
+			System.out.print("Digite o valor da hora trabalhada: R$ ");
+			precoHora = sc.nextDouble();
+
+			System.out.print("Digite a quantidade de dias trabalhados no mês: ");
+			diasTrabalhados = sc.nextInt();
+
+			salarioBruto = horasDia * precoHora * diasTrabalhados;
+
+			desconto = salarioBruto * 0.21;
+
+			salarioLiquido = salarioBruto - desconto;
+
+			System.out.println("Seu salário líquido é R$ " + salarioLiquido);
+
+		}
 
 	}
 
